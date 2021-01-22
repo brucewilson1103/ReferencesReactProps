20f55b96dbd9d08f95ac8283953a9c5798049b4b
Bruce Wilson <brucewilson1103@gmail.com>
2021-01-21 22:11:32 -0500

    add a map to ref

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,32 +3,24 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function newCard(contact) {
+  return (
+    <Card
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+      testimony={contact.testimony}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My References</h1>
       <Avatar image="http://tse3.mm.bing.net/th?id=OIP.gnGLXPYyU31ELk9gdJgkNQHaHa&pid=Api&P=0&w=300&h=300" />
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-        testimony={contacts[0].testimony}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-        testimony={contacts[1].testimony}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-        testimony={contacts[2].testimony}
-      />
+      {contacts.map(newCard)}
     </div>
   );
 }
