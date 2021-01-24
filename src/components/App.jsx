9df9ee162762b310c206728b96e2b323b3d53095
Bruce Wilson <brucewilson1103@gmail.com>
2021-01-23 21:17:59 -0500

    arrow function replace

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,25 +3,21 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
-function newCard(contact) {
-  return (
-    <Card
-      key={contact.id}
-      name={contact.name}
-      img={contact.imgURL}
-      tel={contact.phone}
-      email={contact.email}
-      testimony={contact.testimony}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
       <h1 className="heading">My References</h1>
       <Avatar image="http://tse3.mm.bing.net/th?id=OIP.gnGLXPYyU31ELk9gdJgkNQHaHa&pid=Api&P=0&w=300&h=300" />
-      {contacts.map(newCard)}
+      {contacts.map((contact) => (
+        <Card
+          key={contact.id}
+          name={contact.name}
+          img={contact.imgURL}
+          tel={contact.phone}
+          email={contact.email}
+          testimony={contact.testimony}
+        />
+      ))}
     </div>
   );
 }
